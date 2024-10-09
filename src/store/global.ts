2d5330d097c2b0c6eb95a4ed3bcee6d948695d90
Shlok Main <shlokjagtap123@gmail.com>
2024-10-10 04:06:12 +0530

    build errors fixed

--- src/store/global.ts
+++ src/store/global.ts
@@ -1,7 +1,6 @@
-import { GridwiseProgram } from "@/app/types/gridwise_program";
-import { Idl } from "@coral-xyz/anchor";
+// import { GridwiseProgram } from "@/app/types/gridwise_program";
+// import { Idl } from "@coral-xyz/anchor";
 import { Program } from "@project-serum/anchor";
 import { atom } from "jotai";
-
 
 export const programAtom = atom<Program | null>(null);
